67abbf8b29007b2c311d4b0353a5f591717de4f1
xihajuan2010@gmail.com <xihajuan2010@gmail.com>
2016-12-29 15:55:54 +0800

    temp backup

--- down_t66y.py
+++ down_t66y.py
@@ -13,16 +13,20 @@ from threading import Thread, Lock
 
 from PIL import Image
 
-base_dir=u"Down/"
-temp_i=0
-dir_name = ""
-image_type= ["&jpg"]
-total = 0
-downloaded = 0
-percent = 0
-mutex = Lock()
 
+###### global variables start ######
+g_base_dir=u"Down/"
+g_temp_image_num=0
+g_current_dir = ""
+g_image_type= ["&jpg"]
+g_total_image_count = 0
+g_downloaded = 0
+g_downloaded_percentage = 0
+g_thread_lock = Lock()
+g_max_thread_count = 12
+###### global variables end  ######
 
+###### functions start ######
 def verify_image(image_name):
     v_image = Image.open(image_name)
     if not v_image.verify():
@@ -30,16 +34,14 @@ def verify_image(image_name):
     else:
         return False
         
-
-
-if os.path.exists(base_dir):
+if os.path.exists(g_base_dir):
     if os.name == 'nt':
-        # os.removedirs(dir_name)
-        shutil.rmtree(base_dir)
+        # os.removedirs(g_current_dir)
+        shutil.rmtree(g_base_dir)
     else:
         pass
 else:
-    os.mkdir(base_dir)
+    os.mkdir(g_base_dir)
 # data = []
 # with open('C:\Users\\xihaj\PycharmProjects\\test\DaGaiErDeQiZhi.jsonlines') as f:
 #     for line in f:
@@ -61,17 +63,17 @@ def get_ext(url):
 print sys.argv[1]
 
 def fetch_url(url):
-    global downloaded
-    global percent
+    global g_downloaded
+    global g_downloaded_percentage
     (return_code, name) = check_if_to_downloas(url)
     if return_code == 0:
         try:
             myopener = MyOpener()
             print "downloading",url
             myopener.retrieve(url,name)
-            downloaded = downloaded+1
-            percent = (downloaded*100/total)
-            print "%s%%  downloaded  %s/%s" % (percent, downloaded, total)
+            g_downloaded = g_downloaded+1
+            g_downloaded_percentage = (g_downloaded*100/g_total_image_count)
+            print "%s%%  g_downloaded  %s/%s" % (g_downloaded_percentage, g_downloaded, g_total_image_count)
         except Exception, e:
             print "myopener EORROR name:" + name +" ERROR_CODE:"+":"+unicode(e) 
     else:
@@ -79,7 +81,7 @@ def fetch_url(url):
     # print "'%s\' fetched in %ss" % (url, (time.time() - start))
 
 def threading_download(image_list):
-    global temp_i
+    global g_temp_image_num
     threads = [Thread(target=fetch_url, args=(url,)) for url in image_list]
     for thread in threads:
         # thread.daemon = True
@@ -88,35 +90,49 @@ def threading_download(image_list):
         thread.join()
     # while threading.active_count() > 0:
         # time.sleep(0.1)
+		
+		
+def threading_download2(image_list):	
+	while True:	
+		count = threading.activeCount()
+		if count < g_max_thread_count:
+			if temp_count < len(image_list):
+				threads = threading.Thread(target=target, args=(image_list[temp_count],))
+				threads.start()
+				print "active thread count  == " + str(count)
+			if temp_count  == len(image_list):
+				temp_count = 0
+				#break
+
 
 def check_if_to_downloas(image):
-    global temp_i
-    global downloaded
-    global dir_name
-    mutex.acquire()
-    # temp_i += 1
-    i = temp_i
-    mutex.release()
+    global g_temp_image_num
+    global g_downloaded
+    global g_current_dir
+    g_thread_lock.acquire()
+    # g_temp_image_num += 1
+    i = g_temp_image_num
+    g_thread_lock.release()
     ext = str(image[-4:])
-    if ext in image_type:
+    if ext in g_image_type:
         pass
     else:
-        image_type.append(ext)
+        g_image_type.append(ext)
     ext= str(get_ext(image)) ## get image type
     if str(ext) == ".php" or str(image[-4:]) =="&jpg":
         ext=".jpg"
-    name= dir_name + "/"+ str(i) +ext
+    name= g_current_dir + "/"+ str(i) +ext
     # print "downloading",image
     if os.path.exists(name):
         if  verify_image(name): # image ok
             print "###############verified"
-            downloaded = downloaded+1
+            g_downloaded = g_downloaded+1
             return -1, ""
         else:
             os.remove(name)
             return 0, name
     else:
-        temp_i += 1
+        g_temp_image_num += 1
         return 0, name
       
 with jsonlines.open(sys.argv[1]+'.jsonlines') as reader:
@@ -124,35 +140,36 @@ with jsonlines.open(sys.argv[1]+'.jsonlines') as reader:
         image_list = obj["t_image_list"] # get image list
         if image_list: # if image list not NULL
             for image in image_list:
-                total=total+1
-						
+                g_total_image_count=g_total_image_count+1
+		
 def main():
-    global image_type
-    global dir_name
-    global temp_i
+    global g_image_type
+    global g_current_dir
+    global g_temp_image_num
     with jsonlines.open(sys.argv[1]+'.jsonlines') as reader:
         for obj in reader:
-            dir_name=base_dir+ u''.join(e for e in obj["t_title"] if e.isalnum())  # remove special character for the name and path
-            # print dir_name
-            if dir_name.endswith("poweredbyphpwindnet"):  # remove php title
-                dir_name=dir_name[:-19]
-                if len(dir_name) > 100:
-                    dir_name=dir_name[:100]
-            print dir_name
-            if os.path.exists(dir_name): # if dir not exist, make it
+            g_current_dir=g_base_dir+ u''.join(e for e in obj["t_title"] if e.isalnum())  # remove special character for the name and path
+            # print g_current_dir
+            if g_current_dir.endswith("poweredbyphpwindnet"):  # remove php title
+                g_current_dir=g_current_dir[:-19]
+                if len(g_current_dir) > 100:
+                    g_current_dir=g_current_dir[:100]
+            print g_current_dir
+            if os.path.exists(g_current_dir): # if dir not exist, make it
                 if os.name=='nt':
-                    shutil.rmtree(dir_name)
+                    shutil.rmtree(g_current_dir)
             else:
-                os.mkdir(dir_name)
+                os.mkdir(g_current_dir)
             image_list = obj["t_image_list"] # get image list
             
             if image_list: # if image list not NULL
-                temp_i = 0
+                g_temp_image_num = 0
                 threading_download(image_list)
             else:
                 pass
-            dir_name = ""
-    print image_type
+            g_current_dir = ""
+    print g_image_type
+###### functions end ######	
 
 if __name__ == "__main__":
     main()
