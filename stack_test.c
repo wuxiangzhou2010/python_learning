20e73ba4d0a495c9894ae9686a47c4c53c9de35a
xihajuan2010 <xihajuan2010@gmail.com>
2017-03-23 19:47:21 +0800

    add stack_test.c

---
+++ stack_test.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/* stack */
+
+int * stack_pop(int * stack)
+{
+
+}
+
+int * stack_push(int *stack)
+{
+
+}
+
+int * stack_create(void)
+{
+
+
+}
+
+
+int main(int argc, char *argv[])
+{
+
+	int * stack = stack_create();
+
+}
