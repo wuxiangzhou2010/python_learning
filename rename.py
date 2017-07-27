638806658667a5fec15b1442aad22c9ab68ccf15
xihajuan2010 <xihajuan2010@gmail.com>
2017-07-27 22:10:36 +0800

    rename files in a path

--- rename.py
+++ rename.py
@@ -1,23 +1,34 @@
 import os
 import time
 
-path = os.getcwd()
-# filenames = os.listdir(path)
-
+# path = os.getcwd()
+path = '/home/wuxiang/syno_share/download'
+filenames = os.listdir(path)
+#
 # for filename in filenames:
-    # print filename
-    # print str(filename[-4:])
-    # if str(filename[-4:]) == ".php":
-        # os.rename(filename,filename[:-4]+".jpg")
-        # print("wuxiang############")
-       
-    # else:
-        # pass
-import os
-for root, dirs, files in os.walk(path):
-    for file in files:
-        if file.endswith("poweredbyphpwindnet"):
-            abs_name=os.path.join(root, file)
-            print(os.path.join(root, file))
-            # os.rename(abs_name,abs_name[:-4]+".jpg")
-            os.rename(abs_name,abs_name[:-19])
+#     print filename
+#     print str(filename[-4:])
+#     if str(filename[-4:]) == ".php":
+#         os.rename(filename,filename[:-4]+".jpg")
+#         print("test############")
+#
+#     else:
+#         pass
+# import os
+# for root, dirs, files in os.walk(path):
+#     for file in files:
+#         if file.endswith("poweredbyphpwindnet"):
+#             abs_name=os.path.join(root, file)
+#             print(os.path.join(root, file))
+#             # os.rename(abs_name,abs_name[:-4]+".jpg")
+#             os.rename(abs_name,abs_name[:-19])
+
+
+# remove the download before the file name: eg downloadxxx.jpgc-> xxxx.jpg
+for file in filenames:
+    print(file)
+    test = file[0:7]
+    print(id(test))
+    if str(file[0:7]) == 'download':
+        print(file[7:])
+        os.rename(os.path.join(path, file), os.path.join(path, str(file[7:])))
